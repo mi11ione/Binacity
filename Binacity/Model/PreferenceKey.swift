09c0ff556dfdba19ca6dd3e0b83ad2cfc3ba857f
mi11ione <dev@mi11ioned.com>
2024-02-10 01:05:03 +0300

    [refactor]: cleaned preference keys

--- Binacity/Model/PreferenceKey.swift
+++ Binacity/Model/PreferenceKey.swift
@@ -7,20 +7,6 @@
 
 import SwiftUI
 
-struct ScrollPreferenceKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = nextValue()
-    }
-}
-
-struct CirclePreferenceKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = nextValue()
-    }
-}
-
 struct TabPreferenceKey: PreferenceKey {
     static var defaultValue: CGFloat = 0
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
